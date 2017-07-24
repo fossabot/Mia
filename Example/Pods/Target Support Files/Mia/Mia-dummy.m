c345d9a1fda32542f798da025888927950ef649e
Multinerd <multinerd@users.noreply.github.com>
2017-07-24 00:19:48 -0400

    Removed Pods Folder

--- Example/Pods/Target Support Files/Mia/Mia-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Mia : NSObject
-@end
-@implementation PodsDummy_Mia
-@end
--- Example/Pods/Target Support Files/Pods-Mia_ExampleTests/Pods-Mia_ExampleTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Mia_ExampleTests : NSObject
-@end
-@implementation PodsDummy_Pods_Mia_ExampleTests
-@end
