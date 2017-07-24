6272cacd5975dc8416052e95d13f8c1a27d7f537
Multinerd <multinerd@users.noreply.github.com>
2017-07-24 01:27:59 -0400

    Updated Rosewood Tests

--- Example/Mia_ExampleTests/Rosewood_Test.swift
+++ Example/Mia_ExampleTests/Rosewood_Test.swift
@@ -4,48 +4,44 @@ import Mia
 class Mia_RosewoodTests: XCTestCase {
     
     func testRosewoodLog() -> Void {
-        
-        let rosewood = Rosewood.shared
-        
-        rosewood.verbose("Who am I?: ", self)
-        rosewood.debug("1 + 1 = ", 1 + 1)
-        rosewood.info("IsMainThread: ", Thread.current.isMainThread)
-        rosewood.warning(1, 2, "3", separator: " - ")
-        rosewood.error("Error: ", NSError.init(domain: "Error", code: -999, userInfo: [ "Hello": "World", "Number": 0 ]))
+
+        Rosewood.verbose("Who am I?: ", self)
+        Rosewood.debug("1 + 1 = ", 1 + 1)
+        Rosewood.info("IsMainThread: ", Thread.current.isMainThread)
+        Rosewood.warning(1, 2, "3", separator: " - ")
+        Rosewood.error("Error: ", NSError.init(domain: "Error", code: -999, userInfo: [ "Hello": "World", "Number": 0 ]))
         Thread.sleep(forTimeInterval: 1.0)
         
         Rosewood.Configuration.formatter = .oneline
-        rosewood.verbose("Who am I?: ", self)
-        rosewood.debug("1 + 1 = ", 1 + 1)
-        rosewood.info("IsMainThread: ", Thread.current.isMainThread)
-        rosewood.warning(1, 2, "3", separator: " - ")
-        rosewood.error("Error: ", NSError.init(domain: "Error", code: -999, userInfo: [ "Hello": "World", "Number": 0 ]))
+        Rosewood.verbose("Who am I?: ", self)
+        Rosewood.debug("1 + 1 = ", 1 + 1)
+        Rosewood.info("IsMainThread: ", Thread.current.isMainThread)
+        Rosewood.warning(1, 2, "3", separator: " - ")
+        Rosewood.error("Error: ", NSError.init(domain: "Error", code: -999, userInfo: [ "Hello": "World", "Number": 0 ]))
         Thread.sleep(forTimeInterval: 1.0)
         
         Rosewood.Configuration.formatter = .detailed
-        rosewood.verbose("Who am I?: ", self)
-        rosewood.debug("1 + 1 = ", 1 + 1)
-        rosewood.info("IsMainThread: ", Thread.current.isMainThread)
-        rosewood.warning(1, 2, "3", separator: " - ")
-        rosewood.error("Error: ", NSError.init(domain: "Error", code: -999, userInfo: [ "Hello": "World", "Number": 0 ]))
+        Rosewood.verbose("Who am I?: ", self)
+        Rosewood.debug("1 + 1 = ", 1 + 1)
+        Rosewood.info("IsMainThread: ", Thread.current.isMainThread)
+        Rosewood.warning(1, 2, "3", separator: " - ")
+        Rosewood.error("Error: ", NSError.init(domain: "Error", code: -999, userInfo: [ "Hello": "World", "Number": 0 ]))
         Thread.sleep(forTimeInterval: 1.0)
         
     }
     
     func testRosewoodPrettyPrint() -> Void {
         
-        let rosewood = Rosewood.shared
+        Rosewood.prettyprint(nil)
+        Rosewood.prettyprint(Int(5))
+        Rosewood.prettyprint(Double(5.0))
+        Rosewood.prettyprint(Float(2.0))
+        Rosewood.prettyprint(String("Hello"))
+        Rosewood.prettyprint(Bool(true))
         
-        rosewood.prettyprint(nil)
-        rosewood.prettyprint(Int(5))
-        rosewood.prettyprint(Double(5.0))
-        rosewood.prettyprint(Float(2.0))
-        rosewood.prettyprint(String("Hello"))
-        rosewood.prettyprint(Bool(true))
-        
-        rosewood.prettyprint(["Hello", "World"])
-        rosewood.prettyprint(["Hello": "World", "Number": 0])
-        rosewood.prettyprint(NSError(domain: "Hello, World!", code: 404, userInfo: ["Hello": "World", "Number": 0]))
+        Rosewood.prettyprint(["Hello", "World"])
+        Rosewood.prettyprint(["Hello": "World", "Number": 0])
+        Rosewood.prettyprint(NSError(domain: "Hello, World!", code: 404, userInfo: ["Hello": "World", "Number": 0]))
         //        rosewood.prettyprint(Bool(true)) // NSObject
         
         Thread.sleep(forTimeInterval: 1.0)
@@ -68,13 +64,12 @@ class Mia_RosewoodTests: XCTestCase {
     
     
         func testRosewoodBenchmark() -> Void {
-            let rosewood = Rosewood.shared
+
+            Rosewood.benchmark("Tough Math 1", block: longtask)
     
-            rosewood.benchmark("Tough Math 1", block: longtask)
+            Rosewood.benchmark("Tough Math 10", iterations: 5, block: longtask)
     
-            rosewood.benchmark("Tough Math 10", iterations: 5, block: longtask)
-    
-            rosewood.benchmark("Tough Math Inline", block: {
+            Rosewood.benchmark("Tough Math Inline", block: {
                 longtask()
                 rosewood.benchmarkLog(message: "Hello", includeTimeStamp: true)
                 longtask()
