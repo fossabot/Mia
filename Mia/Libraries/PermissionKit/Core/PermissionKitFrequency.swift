7de5c3fc58c4766328b4cf83772406989571bf69
Multinerd <multinerd@users.noreply.github.com>
2017-09-21 17:07:40 -0400

    Moved permissionskit to seperate repo to take advantage of subspecs

--- Mia/Libraries/PermissionKit/Core/PermissionKitFrequency.swift
+++
@@ -1,8 +0,0 @@
-/// Frequency to ask the user to present the request again
-public enum PermissionKitFrequency {
-    case always
-    case everyHour
-    case onceADay
-    case onceAWeek
-    case justOnce
-}
--- Mia/Libraries/PermissionKit/Core/PermissionKitStatus.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-public enum PermissionKitStatus {
-    case authorized
-    case denied
-    case notDetermined
-    case notAvailable
-}
