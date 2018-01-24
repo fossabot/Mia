aa1fbb79090c1361079c7ca63593eecbaca5fad2
Multinerd <multinerd@users.noreply.github.com>
2018-01-23 22:42:29 -0500

    added build script to generate constants

---
+++ Example/Mia/Constants.swift
@@ -0,0 +1,27 @@
+// Auto generated file from SBConstants - any changes may be lost
+
+public enum StoryboardNames : String {
+    case CameraKit
+    case CollapsibleTableViewDemo
+    case FontKitDemo
+    case Storyboard
+    case ViewControllerInitializerDemo
+    case WebViewDemo
+}
+
+public enum TableViewCellReuseIdentifier : String {
+    case BasicCell
+}
+
+public enum ViewControllerStoryboardIdentifier : String {
+    case BlueViewController
+    case GreenViewController
+    case RedViewController
+    case StoryboardExampleViewController
+    case ViewControllerInitializerDemoViews
+}
+
+public enum XibNames : String {
+    case LaunchScreen
+    case OrangeViewController
+}
